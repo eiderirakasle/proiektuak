68f139e62c70f099464c3eae5255cc4244d32413
Eider <Eider@DESKTOP-GCNANB9>
2019-11-12 14:18:52 +0100

    --2 commit

--- src/rueba.java
+++ src/rueba.java
@@ -4,6 +4,7 @@ public class rueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("kaixo mundua.");
+		System.out.println("2.commit");
 	}
 
 }
